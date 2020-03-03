c50c40f17c1bd24e0b8a139f2c4682a3309cdbdd
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-03 17:26:35 +0000

    Query to select the total requests a teacher got

---
+++ 4_queries/1_total_teacher_requests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(assistance_requests.*) as total_assistances, name
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name
+;
